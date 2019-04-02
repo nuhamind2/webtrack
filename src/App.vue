b8dbd61ab8620b6a43c482621812f9492fea132b
Arina Nuha <nuhamind2@gmail.com>
2019-04-02 10:45:24 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import TrackingMap from "./components/TrackingMap.vue";
+import InputForm from "./components/InputForm.vue";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app"
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  margin:0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import InputForm from './components/InputForm.vue'
+import TrackingMap from './components/TrackingMap.vue'
+import 'leaflet/dist/leaflet.css'
+
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', redirect: '/input'},
+  { path: '/input', component: InputForm },
+  { path: '/track', component: TrackingMap }
+]
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
 
 Vue.config.productionTip = false
 
+document.title = "Lacak GPS"
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
