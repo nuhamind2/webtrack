892483886925d5b55665baae7a4951faedb8c2ae
Arina Nuha <nuhamind2@gmail.com>
2019-04-04 01:24:44 +0700

    add prod dev switch

--- src/api-services/event.service.js
+++ src/api-services/event.service.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
-axios.defaults.baseURL = "http://localhost:3000/"
+
+const isProd = process.env.NODE_ENV === 'production'
+
+axios.defaults.baseURL = isProd ? 'https://api.pirantiempuk.com' : 'http://127.0.0.1:10002'
+console.log(axios.defaults.baseURL)
 
 export default{
     check(eventcode){
