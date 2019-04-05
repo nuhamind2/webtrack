2688008ed2d66cd2141874e0ecb303d7919133b5
Arina Nuha <nuhamind2@gmail.com>
2019-04-05 11:44:32 +0700

    add redirect

--- src/web-server.js
+++ src/web-server.js
@@ -27,7 +27,17 @@ const provision = async () => {
 
     server.route({
         method: 'GET',
+        path: '/',
+        vhost:["pirantiempuk.com","www.pirantiempuk.com"],
+        handler: function (request, h) {
+            return h.redirect("https://dash.pirantiempuk.com")
+        }
+    });
+
+    server.route({
+        method: 'GET',
         path: '/{param*}',
+        vhost:"dash.pirantiempuk.com",
         handler: {
             directory: {
                 path: '.',
